332da2752b581a8dcbf877c226f8601f01a65e36
kcj3054 <kcj3054@naver.com>
2021-04-03 21:22:32 +0900

    complete RateDiscountPolicyTest

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,9 +1,12 @@
 package hello.core.discount;
 
+import hello.core.member.Grade;
+import hello.core.member.Member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
@@ -14,11 +17,23 @@ class RateDiscountPolicyTest {
     @DisplayName("VIP는 10% 할인이 적용되어야 한다 ")
     void vip_o() {
         //given
-
+        Member member = new Member(1L,"memberA", Grade.VIP);
         //when
-
+        int discount = rateDiscountPolicy.discount(member, 10000);
         //that
-        Assertions.assertThat();
+        assertThat(discount).isEqualTo(1000);
+        //assertThat .. import static하는것이 좋다
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야한다")
+    void vip_x() {
+        //given
+        Member member = new Member(1L,"memberA", Grade.BASIC);
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+        //that
+        assertThat(discount).isEqualTo(0);
     }
 
 }
