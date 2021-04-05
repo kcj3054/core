ec6fb957a58db030cd8a93916e49b68af25cd62e
kcj3054 <kcj3054@naver.com>
2021-04-06 00:18:38 +0900

    의존관계 주입

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     //추상화, 구체화 둘다에 의존한다 DIP를 위반하는 것이다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public MemberServiceImpl(MemoryMemberRepository memoryMemberRepository) {
+        this.memberRepository = memoryMemberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,22 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements  OrderService{
+    private  final MemberRepository memberRepository;    //private  final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private  final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
-    private  final MemberRepository memberRepository = new MemoryMemberRepository();
-    private  final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private  final DiscountPolicy discountPolicy ;  //dip 위반 하지않도록 (인터페이스에만 의존)
+    //누군가 discountPolicy에 구현 객체를 주입 해주어야한다
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order CreateOrder(Long memberId, String itemName, int itemPrice) {
