5c528119c92ba39a7179976f963934a4ddf207c7
20161351 <kcj3054@naver.com>
2021-04-08 20:32:09 +0900

    dip, ocp 성공

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,12 +11,20 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-
+    //CTRL + ALT + N
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() { //역할
+        //return new FixDiscountPolicy(); //구현
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,9 +3,9 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     //추상화, 구체화 둘다에 의존한다 DIP를 위반하는 것이다.
-    private MemberRepository memberRepository = new MemoryMemberRepository();
+    private MemberRepository memberRepository;
 
-    public MemberServiceImpl(MemoryMemberRepository memoryMemberRepository) {
+    public MemberServiceImpl(MemberRepository memoryMemberRepository) {
         this.memberRepository = memoryMemberRepository;
     }
 
