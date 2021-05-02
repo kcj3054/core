dfcbff03efe88973d7cefa5d600eb65a2366b56a
kcj3054 <kcj3054@naver.com>
2021-05-02 10:51:22 +0900

    BeanLifeCycle

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,29 +6,19 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-//@RequiredArgsConstructor
-@Getter
-@Setter
-
 public class OrderServiceImpl implements  OrderService{
     private  final MemberRepository memberRepository;    //private  final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private  final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
-    private final DiscountPolicy discountPolicy ;  //dip 위반 하지않도록 (인터페이스에만 의존)
+    private  final DiscountPolicy discountPolicy ;  //dip 위반 하지않도록 (인터페이스에만 의존)
     //누군가 discountPolicy에 구현 객체를 주입 해주어야한다
 
-    //RequiredArgsConstructor 사용시 자동으로 final 키워드 붙은거 생성자 만들어준다
-    @Autowired  // 생성자가 하나면 Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository,
-                            @Qualifier("miainDiscountPolicy") DiscountPolicy discountPolicy) {
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
